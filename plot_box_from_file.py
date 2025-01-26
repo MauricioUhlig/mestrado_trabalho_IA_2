915f2ed60c8ef9a0992f5f4109d81b745af786b6
Mauricio Uhlig <mauricio.uhlig@trustly.com>
2025-01-26 18:44:46 -0300

    Atualizacao dos boxplot

--- plot_box_from_file.py
+++ plot_box_from_file.py
@@ -1,15 +1,15 @@
 import pandas as pd
 import plots
 
-df = pd.read_csv('df_hit_func_objetivo_tsp.csv', index_col='ALGORITMO')
+dados = [
+            ['df_custos_tsp.csv','Custo dos algorítmos'],
+            ['df_hit_func_objetivo_tsp.csv','Quantidade de acessos na função objetivo'],
+            ['df_tempo_tsp.csv','Tempo de execução (ms)'],
+            ['df_custos_rastrigin.csv','Custo dos algorítmos'],
+            ['df_hit_func_objetivo_rastrigin.csv','Quantidade de acessos na função objetivo'],
+            ['df_tempo_rastrigin.csv','Tempo de execução (ms)'],
+        ]
 
-
-
-plots.boxplot_sorted(df,"Quantidade de acessos na função objetivo")
-
-df = pd.read_csv('df_hit_func_objetivo_rastrigin.csv', index_col='ALGORITMO')
-
-
-
-plots.boxplot_sorted(df,"Quantidade de acessos na função objetivo")
-
+for file, title in dados:
+    df = pd.read_csv(file, index_col='ALGORITMO')
+    plots.boxplot_sorted(df,title)
